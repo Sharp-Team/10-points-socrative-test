0b86af52efd009db70706bcd2527d24d5aa1e4f2
Demons Light <jetaimefrc@gmail.com>
2019-10-30 08:30:31 +0700

    feat: update fetch answer mutiple choice

--- 10pointsEz.js
+++ 10pointsEz.js
@@ -1,20 +1,21 @@
 const fs = require('fs')
-const data = require('./data.json')
+const data = require('./data_test3.json')
 const questions = data.questions
 
 const result = []
 // Thêm ID để chia ra => Mỗi thành viên sẽ có result.length / n(số thành viên) câu hỏi => Ez game!!!
 let id = 0
 // Lưu kết quả vào file
-const fileResult = 'result.json'
+const fileResult = 'result2.json'
 
 questions.forEach(({ question_text, answers }) => {
   const temp = {}
   const res = answers.filter(({ is_correct }) => is_correct === true)
   temp.id = id
   // Vì câu hỏi đôi khi sẽ dài => cắt ngắn cho dễ nhìn!
-  temp.question = question_text.substring(0, 60)
-  temp.answer = res[0].text
+  // temp.question = question_text.substring(0, 60)
+  temp.question = question_text
+  temp.answer = res
   result.push(temp)
   id++
 })
@@ -23,3 +24,5 @@ fs.writeFile(fileResult, JSON.stringify(result, null, 2), err => {
   if (err) throw err
   console.log('Result saved!')
 })
+
+// https://api.socrative.com/quizzes/api/quiz/37418947?room_name=FALL19THAOGTM
